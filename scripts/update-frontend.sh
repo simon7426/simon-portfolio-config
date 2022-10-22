e736b9b2557a99e05acae7faa6f9ea2227c3f350
simon.islam <simon.islam@brilliant.com.bd>
2022-10-23 02:14:24 +0600

    updated script

--- scripts/update-frontend.sh
+++ scripts/update-frontend.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-ls -la
-pwd
+set -e
+
+GITHUB_SHA_SHORT=$(echo $1 | cut -c1-7)
+DOCKER_USERNAME=$(echo $2 | base64 -d)
+
+cd portfolio/portfolio-frontend
+
+kustomize edit set image $DOCKER_USERNAME/vue-portfolio-kubernetes:$GITHUB_SHA_SHORT
+
+kustomize build > portfolio-frontend-build/portfolio-frontend-kustomize.yaml
